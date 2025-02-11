023de9aff2f4f3ca2403fc7b5bdfe20f7fd78531
Decrypt <louis.vicomte@protonmail.com>
2025-02-11 10:38:05 +0100

    fix android header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,7 +26,16 @@ export default function RootLayout() {
 				},
 			}}
 		>
-			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			<Stack.Screen
+				name="(tabs)"
+				options={{
+					headerShown: Platform.select({
+						android: true,
+						ios: false,
+					}),
+					headerTitle: "",
+				}}
+			/>
 
 			<Stack.Screen
 				name="search"
